acdc5f1054dad794bde9e4dd7558184f908e0eba
Zach Barbre <zach.barbre@gmail.com>
2020-04-07 10:28:08 -0400

    add post and delete to the model

--- models/guessesModel.js
+++ models/guessesModel.js
@@ -13,7 +13,32 @@ class GuessModel {
             const response = await db.any(`SELECT * FROM guesses;`);
             return response;
         } catch (error) {
-            console.log(error);
+            console.error(error);
+            return error;
+        }
+    }
+
+    static async postGuess(name, birthdate, weight, sex){
+        try {
+            const response = await db.one(
+                `INSERT INTO guesses (name, birthdate, weight, sex)
+                VALUSE ($1, $2, $3, $4)`,[name, birthdate, weight, sex]
+            );
+            return response;
+        } catch (error) {
+            console.error(error);
+            return error;
+        }
+    }
+
+    static async deleteGuess(id){
+        try {
+            const response = await db.result(
+                `DELETE FROM guesses WHERE id = $1`,[id]
+            );
+            return response;
+        } catch (error) {
+            console.error(error);
             return error;
         }
     }
