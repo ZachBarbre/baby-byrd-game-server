275b5cf2ff6a329973e60c43556bdf886c254d0d
Zach Barbre <zach.barbre@gmail.com>
2020-05-12 10:27:26 -0400

    add post and delete routes

--- routes/guesses.js
+++ routes/guesses.js
@@ -9,4 +9,32 @@ router.get('/', async (req, res) => {
   res.json(guesses);
 });
 
+router.post('/', async (req, res) => {
+  const newGuess = new GuessesModel({
+    name: req.body.name,
+    babyWeight: req.body.babyWeight,
+    birthDate: req.body.birthDate,
+    sex: req.body.sex,
+  });
+  try {
+    const guess = await newGuess.save();
+    console.log(guess);
+    res.json(guess);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+});
+
+router.delete('/:id', async (req, res) =>{
+  try {
+    const guess = await GuessesModel.findById(req.params.id);
+    const deletedGuess = await guess.remove();
+    res.json({sucess: true});
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({sucess: false});
+  }
+});
+
 module.exports = router;
